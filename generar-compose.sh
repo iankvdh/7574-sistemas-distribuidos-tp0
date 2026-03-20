fbf10ee589341621d8b2806f4b545d2a4476ad5c
iankvdh <ivon@fi.uba.ar>
2026-03-20 08:44:52 -0300

    fix (ej3): se permite que generar-compose.sh reciba 0 clientes (para levantar solo el server)

--- generar-compose.sh
+++ generar-compose.sh
@@ -11,11 +11,6 @@ fi
 archivo_salida="$1"
 cantidad_clientes="$2"
 
-if ! [[ "$cantidad_clientes" =~ ^[1-9][0-9]*$ ]]; then
-    echo "Error: <cantidad_clientes> debe ser un entero positivo (>= 1)."
-    exit 1
-fi
-
 echo "Nombre del archivo de salida: $archivo_salida"
 echo "Cantidad de clientes: $cantidad_clientes"
 
