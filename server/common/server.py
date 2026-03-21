0bc8fc13cea8c2938c2f7273d7d83447a63e678b
iankvdh <ivon@fi.uba.ar>
2026-03-21 17:01:42 -0300

    fix (ej4): vuelvo atras por falla de tests

--- server/common/server.py
+++ server/common/server.py
@@ -34,10 +34,7 @@ class Server:
                 self.__handle_client_connection(client_sock)
                 self._clients.remove(client_sock)
             except OSError as e:
-                if self._running:
-                    logging.error(f"action: accept_connections | result: fail | error: {e}")
-                else:
-                    logging.info(f"action: accept_connections | result: interrupted | error: {e}")
+                logging.error(f"action: accept_connections | result: fail | error: {e}")
                 break
 
         for client in self._clients:
